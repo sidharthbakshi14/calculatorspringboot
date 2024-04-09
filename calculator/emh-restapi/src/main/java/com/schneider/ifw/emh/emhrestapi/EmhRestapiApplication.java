39d3e7d814a0c0a5d2b4dd145ce5d1ccd9b72d9f
sidharthbakshi14 <76621093+sidharthbakshi14@users.noreply.github.com>
2024-04-09 10:35:21 +0530

    Add files via upload

---
+++ calculator/emh-restapi/src/main/java/com/schneider/ifw/emh/emhrestapi/EmhRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.schneider.ifw.emh.emhrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmhRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmhRestapiApplication.class, args);
+	}
+
+}
---
+++ calculator/emh-restapi/src/test/java/com/schneider/ifw/emh/emhrestapi/EmhRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schneider.ifw.emh.emhrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmhRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
